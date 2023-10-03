b0bcb9e5be2ffb217b32babf9acd96975e147da1
arjunkareti <79628958+arjunkareti@users.noreply.github.com>
2023-10-03 16:24:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const audioElements = [];
 // Preload audio files
 function preloadAudio() {
   for (let i = 1; i <= 10; i++) {
-    const audio = new Audio(`jump${i}.wav`);
+    const audio = new Audio(`jump${i}.wav?cachebuster=${Math.random()}`);
     audioElements.push(audio);
   }
 }
